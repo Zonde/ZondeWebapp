95e89233335023b53ecd572a4f2f8d422e504083
Thomas Brocken <thomasbrocken@live.nl>
2018-03-14 14:17:03 +0100

    Bugfix

--- zonde/settings.py
+++ zonde/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY_ZONDE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
