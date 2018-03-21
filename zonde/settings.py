0b3134c2ff8cfd05e2e774b5ce879b6bf1d459da
Thomas Brocken <thomasbrocken@live.nl>
2018-03-21 15:54:06 +0100

    Auto debug switch

--- zonde/settings.py
+++ zonde/settings.py
@@ -22,8 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY_ZONDE')
 
+DEBUG = False if 'PROD' in os.environ else True
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
