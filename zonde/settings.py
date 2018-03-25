96204ac707d0e24eaa7a4de4c4744a9073f95665
Thomas Brocken <thomasbrocken@live.nl>
2018-03-25 09:17:07 +0200

    Log

--- zonde/settings.py
+++ zonde/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'zonde.urls'
 
 RQ_QUEUES = {
     'default': {
-        'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
+        'URL': os.getenv('REDISTOGO_URL', os.getenv('REDISTOGO_URL_ZONDE', 'redis://localhost:6379/0')), # If you're on Heroku
         'DEFAULT_TIMEOUT': 500,
     },
 }
