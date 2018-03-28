429b3d5e711633efb108ef517cb08870f68c45de
Daan de Graaf <daandegraaf9@gmail.com>
2018-03-28 16:28:08 +0200

    Ignore duplicate BSSIDs

--- zonde_app/wigle.py
+++ zonde_app/wigle.py
@@ -11,5 +11,9 @@ def ssid_added(ssid):
     print("Looking for ssid: {}".format(ssid))
     networks = search.ssid(TOKEN, ssid)
     for n in networks:
-        Network.objects.create(ssid=ssid, latitude=n.lat, longitude=n.long, bssid=n.bssid)
+        try:
+            Network.objects.create(ssid=ssid, latitude=n.lat, longitude=n.long, bssid=n.bssid)
+        except IntegrityError:
+            # Already exists
+            pass
     print("Added {} networks for ssid {}".format(len(networks), ssid))
